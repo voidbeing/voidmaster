cc87b30be27911c366c4f0f853fdcb691fbf43bc
Void Being <u9662316@oz.nthu.edu.tw>
2019-02-19 22:32:04 +0800

    Add showfft.go
    This program can generate a profile like this:
    
                      |
      original image  |  FFT (real)
                      |
    ------------------+------------------
                      |          FFT real
       FFT (imag)     |
                      | FFT imag

--- watcher/src/showfft.go
+++ watcher/src/showfft.go
@@ -21,51 +21,51 @@ func main() {
 	w := conf.Width
 	h := conf.Height
 
+	var r, g, b uint32
+	var img [3]*dsputils.Matrix
+
 	in.Seek(0, 0)
 	src, _, _ := image.Decode(in)
-	vol := dsputils.MakeEmptyMatrix([]int{w, h})
+	img[0] = dsputils.MakeEmptyMatrix([]int{w, h})
+	img[1] = dsputils.MakeEmptyMatrix([]int{w, h})
+	img[2] = dsputils.MakeEmptyMatrix([]int{w, h})
 
 	for x := 0; x < w; x++ {
 		for y := 0; y < h; y++ {
-			r, _, _, _ := src.At(x, y).RGBA()
-			rc := complex(float64(r), 0)
-			vol.SetValue(rc, []int{x, y})
+			r, g, b, _ = src.At(x, y).RGBA()
+			img[0].SetValue(complex(float64(r), 0), []int{x, y})
+			img[1].SetValue(complex(float64(g), 0), []int{x, y})
+			img[2].SetValue(complex(float64(b), 0), []int{x, y})
 		}
 	}
 
 	// FFT
-	fftorig := fft.FFTN(vol)
+	var fftorig [3]*dsputils.Matrix
 
-	// transform
-	//threashold := 100
-	//for x := 0; x < w; x++ {
-	//	for y := 0; y < h; y++ {
-	//		if (x > threashold && x <= w-threashold-1) && (y > threashold && y < h-1-threashold) {
-	//			fftorig.SetValue(complex(0, 0), []int{0, 0})
-	//		}
-	//	}
-	//}
+	fftorig[0] = fft.FFTN(img[0])
+	fftorig[1] = fft.FFTN(img[1])
+	fftorig[2] = fft.FFTN(img[2])
 
-	maxr := 0.0
-	maxi := 0.0
-	minr := 0.0
-	mini := 0.0
+	// mid-processing
+	var maxmin [4][3]float64
 
 	for x := 0; x < w; x++ {
 		for y := 0; y < h; y++ {
-			fftr := real(fftorig.Value([]int{x, y}))
-			ffti := imag(fftorig.Value([]int{x, y}))
-			if maxr < fftr {
-				maxr = fftr
-			}
-			if minr > fftr {
-				minr = fftr
-			}
-			if maxi < ffti {
-				maxi = ffti
-			}
-			if mini > ffti {
-				mini = ffti
+			for z := 0; z < 3; z++ {
+				fftr := real(fftorig[z].Value([]int{x, y}))
+				ffti := imag(fftorig[z].Value([]int{x, y}))
+				if maxmin[0][z] < fftr {
+					maxmin[0][z] = fftr
+				}
+				if maxmin[1][z] > fftr {
+					maxmin[1][z] = fftr
+				}
+				if maxmin[2][z] < ffti {
+					maxmin[2][z] = ffti
+				}
+				if maxmin[3][z] > ffti {
+					maxmin[3][z] = ffti
+				}
 			}
 		}
 	}
@@ -74,13 +74,19 @@ func main() {
 		return uint8(val * 255 / (a - b))
 	}
 
-	fftimg := image.NewRGBA(image.Rect(0, 0, w*2, h))
+	fftimg := image.NewRGBA(image.Rect(0, 0, w*2, h*2))
 	for x := 0; x < w; x++ {
 		for y := 0; y < h; y++ {
-			fftr := linear(real(fftorig.Value([]int{x, y})), maxr, minr)
-			ffti := linear(imag(fftorig.Value([]int{x, y})), maxi, mini)
-			fftimg.Set(x, y, color.RGBA{fftr, ffti, ffti, 255})
-			fftimg.Set(x+w, y, src.At(x, y))
+			var temp [2][3]uint8
+			for z := 0; z < 3; z++ {
+				temp[0][z] = linear(real(fftorig[z].Value([]int{x, y})), maxmin[0][z], maxmin[1][z])
+				temp[1][z] = linear(imag(fftorig[z].Value([]int{x, y})), maxmin[2][z], maxmin[3][z])
+			}
+			fftimg.Set(x, y, src.At(x, y))
+			fftimg.Set(x+w, y, color.RGBA{temp[0][0], temp[0][1], temp[0][2], 255})
+			fftimg.Set(x, y+h, color.RGBA{temp[0][0], temp[0][1], temp[0][2], 255})
+			fftimg.Set(x+w+w/2, y+h, fftimg.At(x/8+w, y/8))
+			fftimg.Set(x+w, y+h+h/2, fftimg.At(x/8, y/8+h))
 		}
 	}
 
